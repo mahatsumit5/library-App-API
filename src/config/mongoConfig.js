9741d67c3e19322b8cd046291a209d036d6d2e38
mahat sumit <mahatsumit5@gmail.com>
2023-07-27 23:15:24 +1000

    build folder changed

--- src/config/mongoConfig.js
+++ src/config/mongoConfig.js
@@ -1,9 +1,13 @@
 import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
+  const dbLink =
+    process.env.NODE_ENV === "production"
+      ? process.env.MONGO_CLIENT
+      : "mongodb://127.0.0.1:27017/librarySystem";
+
   try {
-    console.log(process.env.Node_ENV);
-    const con = await mongoose.connect(process.env.MONGO_CLIENT);
+    const con = await mongoose.connect(dbLink);
     con && console.log("mongo is connected");
   } catch (error) {
     console.log(error);
